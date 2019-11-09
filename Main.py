3a2d48fcfe84f28a20df04da2973015c3589fb47
Sankek <iziurov.as@phystech.edu>
2019-11-09 19:46:16 +0300

    Made a matrix module Matrix.py containing class for matrix initialisation and operations with them. Main.py is used here to provide an example of matrix using for a future implementation of a 3D game.

---
+++ Main.py
@@ -0,0 +1,168 @@
+"""
+It's a base for a future 3D game.
+
+Press 'd' to rotate clockwise
+Press 'a' to rotate counterclockwise
+Press 's' to stop.
+"""
+
+from tkinter import *
+from Matrix import *
+import math
+import time
+
+root = Tk()
+fr = Frame(root)
+root.geometry('800x600')
+root.resizable(False, False)
+canv = Canvas(root, bg='white')
+canv.pack(fill=BOTH, expand=1)
+
+
+def crds_to_row(crds):
+    row = []
+    for matr in crds:
+        row += matr.transpose()[0]
+    return row
+
+
+class Basis:
+    def __init__(self):
+        pass
+
+
+class Rectangle:
+    def __init__(self, x=400, y=300, a=200, b=100, angle=0.0):
+        self.center = Matrix(2, 1, [[x], [y]])
+        self.side_a = a
+        self.side_b = b
+
+        self.crds = [None]*4
+        for i in range(4):
+            self.crds[i] = Matrix(2, 1)
+        self.crds[0].set_col(0, [-a/2, -b/2])
+        self.crds[1].set_col(0, [a/2, -b/2])
+        self.crds[2].set_col(0, [a/2, b/2])
+        self.crds[3].set_col(0, [-a/2, b/2])
+
+        self.rotate(angle)
+
+        self.abscrds = [None]*4
+        for i in range(4):
+            self.abscrds[i] = self.crds[i]
+            self.abscrds[i] += self.center
+
+        self.id = canv.create_polygon(*crds_to_row(self.abscrds),  fill='black')
+
+    def set_coords(self):
+        for i in range(4):
+            self.abscrds[i] = self.crds[i]
+            self.abscrds[i] += self.center
+        canv.coords(self.id, *crds_to_row(self.abscrds))
+
+    def transform(self, func, *args, **kwargs):
+        for i in range(4):
+            self.crds[i] = func(*args, **kwargs)*self.crds[i]
+
+    def rotate(self, angle):
+        self.transform(M_rot, angle)
+
+
+def M_rot(a):
+    new_matr = Matrix(2, 2, [[math.cos(a), math.sin(a)], [-math.sin(a), math.cos(a)]])
+    return new_matr
+
+
+def transform_1():
+    new_matr = Matrix(2, 2, [[2, 0], [0, 1]])
+    return new_matr
+
+
+def transform_2():
+    new_matr = Matrix(2, 2, [[1, 1], [0, 1]])
+    return new_matr
+
+
+# def rot_Mx(a):
+#     m = Matrix(3, 3)
+#     m.set([[1, 0, 0],
+#            [0, math.cos(a), -math.sin(a)],
+#            [0, math.sin(a), math.cos(a)]])
+#     return m
+#
+#
+# def rot_My(a):
+#     m = Matrix(3, 3)
+#     m.set([[math.cos(a), 0, math.sin(a)],
+#            [0, 1, 0],
+#            [-math.sin(a), 0, math.cos(a)]])
+#     return m
+#
+#
+# def rot_Mz(a):
+#     m = Matrix(3, 3)
+#     m.set([[math.cos(a), -math.sin(a), 0],
+#            [math.sin(a), math.cos(a), 0],
+#            [0, 0, 1]])
+#     return m
+#
+#
+# C = rot_Mx(math.pi / 2) * rot_My(math.pi / 2) * rot_Mz(math.pi / 2)
+# C.print()
+# print('-----')
+# coords = Matrix(3, 1)
+# coords.set_col(0, [1, 0, 0])
+# coords.print()
+# print('---')
+# new_coords = C * coords
+# new_coords.print()
+
+
+stopped = rt = lt = None
+
+
+def loop():
+    def stop(event):
+        global stopped
+        stopped = True
+
+    def left(event):
+        global lt, rt
+        lt = True
+        rt = False
+
+    def right(event):
+        global rt, lt
+        rt = True
+        lt = False
+
+    canv.bind_all('s', stop)
+    canv.bind_all('a', left)
+    canv.bind_all('d', right)
+
+    canv.update()
+    time.sleep(1)
+    rect.transform(transform_1)
+    rect.set_coords()
+    canv.update()
+    time.sleep(1)
+    rect.transform(transform_2)
+    rect.set_coords()
+    canv.update()
+    time.sleep(1)
+
+    while stopped is not True:
+        rect.set_coords()
+
+        if rt is True:
+            rect.rotate(-math.pi/180)
+        if lt is True:
+            rect.rotate(math.pi/180)
+        canv.update()
+        time.sleep(0.02)
+
+
+rect = Rectangle(angle=math.pi/2)
+loop()
+
+mainloop()
---
+++ Matrix.py
@@ -0,0 +1,151 @@
+"""
+This module contains main matrix classes and operations with them. Addition, subscraction, multiplication,
+exponentiation and equality operators are defined for appropriate matices.
+
+A mathematical matrix in this module is a list [row_1, row_2, ...], where row_i = [element_in_column_1_of_row_i, ...]
+
+Reference to a matrix element in row 'm' and in column 'n' uses the standard notation A[m][n].
+A[m] refers to the whole row m.
+Note that a nubmer of row or a column starts from the zero.
+
+There are some exmaples in the end of the module.
+"""
+
+
+class Matrix:
+    def __init__(self, rows=0, columns=0, _list=None):
+        self.size = str(rows)+'x'+str(columns)
+        self.rows = rows
+        self.cols = columns
+        if _list is not None:
+            self._list = _list
+        else:
+            self._list = []
+            for r in range(rows):
+                col = []
+                for c in range(columns):
+                    col.append(0)
+                self._list.append(col)
+            
+    def __getitem__(self, row):
+        return self._list[row]
+
+    def __setitem__(self, row, new_row):
+        self._list[row] = new_row
+
+    def print(self):
+        for i in self:
+            print(i)
+
+    def set(self, new_list_or_const):
+        try:
+            for r in range(self.rows):
+                for c in range(self.cols):
+                    self[r][c] = new_list_or_const[r][c]
+        except TypeError:
+            for r in range(self.rows):
+                for c in range(self.cols):
+                    self[r][c] = new_list_or_const
+
+    def set_row(self, row, new_list_or_const):
+        try:
+            self[row] = list(new_list_or_const)
+        except TypeError:
+            for c in range(self.cols):
+                self[row][c] = new_list_or_const
+
+    def set_col(self, col, new_list_or_const):
+        try:
+            for r in range(self.rows):
+                self[r][col] = new_list_or_const[r]
+        except TypeError:
+            for r in range(self.rows):
+                self[r][col] = new_list_or_const
+
+    def __eq__(self, other):
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self[r][c] != other[r][c]:
+                    return False
+        return True
+
+    def __add__(self, other):
+        new_matr = Matrix(self.rows, self.cols)
+        for r in range(self.rows):
+            for c in range(self.cols):
+                new_matr[r][c] = self[r][c]+other[r][c]
+        return new_matr
+
+    def __mul__(self, other_or_const):
+        try:
+            new_matr = Matrix(self.rows, other_or_const.cols)
+            for r in range(self.rows):
+                for c in range(other_or_const.cols):
+                    for n in range(self.cols):
+                        new_matr[r][c] += self[r][n]*other_or_const[n][c]
+            return new_matr
+        except AttributeError:
+            new_matr = self
+            for r in range(self.rows):
+                new_matr[r] = [i*other_or_const for i in self[r]]
+            return new_matr
+
+    def __sub__(self, other):
+        new_matr = self
+        new_matr += other*-1
+        return new_matr
+
+    def __pow__(self, power):
+        if power == 0:
+            new_matr = E(self.rows)
+            return new_matr
+        new_matr = self
+        for i in range(power-1):
+            new_matr *= self
+        return new_matr
+
+    def transpose(self):
+        new_matr = Matrix(self.cols, self.rows)
+        for c in range(self.cols):
+            for r in range(self.rows):
+                new_matr[c][r] = self[r][c]
+        return new_matr
+
+
+def matr_E(n):
+    new_matr = Matrix(n, n)
+    for i in range(n):
+        new_matr[i][i] = 1
+    return new_matr
+
+
+def to_matrix(rows, cols, **kwargs):
+    for i in kwargs:
+        i = Matrix
+
+
+if __name__ == '__main__':
+    print('--- setting a matrix and its size ---')
+    A = Matrix(4, 3)
+    A.print()
+    print('--- setting its values ---')
+    A[0][1] = 5
+    A.set_row(1, [1, 2, 3])
+    A.print()
+    print('--- changing rows ---')
+    A[0], A[1] = A[1], A[0]
+    A.print()
+    print('--- changing elements ---')
+    A[0][2], A[0][0] = A[0][0], A[0][2]
+    A.print()
+    print('--- example of matrix operations ---')
+    A = Matrix(2, 2, [[1, 2], [2, 3]])
+    B = Matrix(2, 2, [[-1, -2], [2, 1]])
+    print('A =')
+    A.print()
+    print('B =')
+    B.print()
+    print('\nA+B =')
+    (A+B).print()
+    print('\n3A*2B =')
+    ((A*3)*(B*2)).print()  # note that to multiply matrix by a constant you have to write A*c not c*A
