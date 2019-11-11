fdfe8184e20c17f007f859dec1e4cacc63242bd9
Sankek <iziurov.as@phystech.edu>
2019-11-12 01:13:59 +0300

    Added movement of the system center but  too many KOSTILEY KOSTILY EVERYWHERE I HAVE TO REWRITE ALL OF THIIIIIIIIIIIIIIIIIIISSSSSSSSSSS

--- Main3D.py
+++ Main3D.py
@@ -1,8 +1,8 @@
 """
 It's like the pseudo 3D.
 
-Press 'a' to start torating like in 3D
-Press 's' to stop.
+Press 'a', 'w', 's', 'd' to move in 4-directions
+Press 'q', 'e' to start rotating like in 3D
 """
 
 from tkinter import *
@@ -46,7 +46,7 @@ class Line(Object):
 
 class Point(Object):
     def __init__(self, crd, x=400, y=300, z=0, color='red', size=2):
-        super().__init__(crd, x, y, z=0)
+        super().__init__(crd, x, y, z)
         self.sizeMatr = Matrix(3, 1, [[size], [size], [size]])
         self.id = canv.create_oval(*(self.abscrds[0]-self.sizeMatr).col(0)[:2],
                                    *(self.abscrds[0]+self.sizeMatr).col(0)[:2], fill=color, width=0)
@@ -58,19 +58,23 @@ class Point(Object):
 
 class Basis3D:
     def __init__(self, x=400, y=300, z=0, length=40):
+        self.center = Matrix(3, 1, [[x], [y], [z]])
         self.x = Line([(0, 0, 0), (length, 0, 0)], x, y, z, color='red')
         self.y = Line([(0, 0, 0), (0, length, 0)], x, y, z, color='green')
         self.z = Line([(0, 0, 0), (0, 0, length)], x, y, z, color='blue')
+        self.x.center = self.y.center = self.z.center = self.center
 
     def set_coords(self):
         self.x.set_coords()
         self.y.set_coords()
         self.z.set_coords()
+        self.x.center = self.y.center = self.z.center = self.center
 
     def transform(self, func, *args, **kwargs):
         self.x.transform(func, *args, **kwargs)
         self.y.transform(func, *args, **kwargs)
         self.z.transform(func, *args, **kwargs)
+        self.x.center = self.y.center = self.z.center = self.center
 
     def rotate(self, angle):
         self.x.rotate(angle)
@@ -85,7 +89,7 @@ class Polygon(Object):
 
 
 class Rectangle(Polygon):
-    def __init__(self, x=400, y=300, a=200, b=100):
+    def __init__(self, x=400, y=300, z=0, a=200, b=100):
         crds = [(-a/2, -b/2, 0), (a/2, -b/2, 0), (a/2, b/2, 0), (-a/2, b/2, 0)]
         super().__init__(crds, x, y, z)
 
@@ -105,6 +109,17 @@ class Link:
 
     def rotate(self, angle):
         self.transform(M_rot, angle)
+
+    def add_to_center(self, matr):
+        for obj in self.objects:
+            obj.center += matr
+            if isinstance(obj, Basis3D):
+                obj.x.abscrds = [matr+obj.x.center for matr in obj.x.crds]
+                obj.y.abscrds = [matr+obj.y.center for matr in obj.y.crds]
+                obj.z.abscrds = [matr+obj.z.center for matr in obj.z.crds]
+            else:
+                obj.abscrds = [matr+obj.center for matr in obj.crds]
+        self.set_coords()
 
 
 def crds_to_row(crds):
@@ -164,25 +179,74 @@ def rot_Mz(a):
     return m
 
 
-stopped, rt, lt = None, True, None
+def bindings():
+    def b_trans1(event):
+        global trans1
+        trans1 = True
+
+    def bs_trans1(event):
+        global trans1
+        trans1 = False
+
+    def b_trans2(event):
+        global trans2
+        trans2 = True
+
+    def bs_trans2(event):
+        global trans2
+        trans2 = False
+
+    def b_left(event):
+        global lt
+        lt = True
+
+    def bs_left(event):
+        global lt
+        lt = False
+
+    def b_right(event):
+        global rt
+        rt = True
+
+    def bs_right(event):
+        global rt
+        rt = False
+
+    def b_up(event):
+        global up
+        up = True
+
+    def bs_up(event):
+        global up
+        up = False
+
+    def b_down(event):
+        global dn
+        dn = True
+
+    def bs_down(event):
+        global dn
+        dn = False
+
+    canv.bind_all('a', b_left)
+    canv.bind_all('<KeyRelease-a>', bs_left)
+    canv.bind_all('d', b_right)
+    canv.bind_all('<KeyRelease-d>', bs_right)
+    canv.bind_all('w', b_up)
+    canv.bind_all('<KeyRelease-w>', bs_up)
+    canv.bind_all('s', b_down)
+    canv.bind_all('<KeyRelease-s>', bs_down)
+    canv.bind_all('q', b_trans1)
+    canv.bind_all('<KeyRelease-q>', bs_trans1)
+    canv.bind_all('e', b_trans2)
+    canv.bind_all('<KeyRelease-e>', bs_trans2)
+
+
+rt, lt, up, dn, trans1, trans2 = False, False, False, False, False, False
+bindings()
 
 
 def loop():
-    def stop(event):
-        global stopped
-        stopped = True
-
-    def left(event):
-        global lt, rt, stopped
-        lt, stopped = True, False
-
-    def right(event):
-        global rt, lt, stopped
-        rt, stopped = True, False
-
-    canv.bind_all('s', stop)
-    canv.bind_all('a', left)
-    canv.bind_all('d', right)
 
     canv.update()
     time.sleep(1)
@@ -191,13 +255,22 @@ def loop():
         pass
 
     def loop_rotation():
-        if stopped is not True:
+        if rt or lt or up or dn or trans1 or trans2 is True:
             new_poly.set_coords()
 
-            if rt is True:
+            if trans1 is True:
                 new_poly.transform(rot_Mx, math.pi/180)
+            if trans2 is True:
+                new_poly.transform(rot_Mz, math.pi/180)
+            if up is True:
+                new_poly.add_to_center(Matrix(3, 1, [[0], [-1], [0]]))
+            if dn is True:
+                new_poly.add_to_center(Matrix(3, 1, [[0], [1], [0]]))
             if lt is True:
-                new_poly.transform(rot_Mz, 0.5*math.pi/180)
+                new_poly.add_to_center(Matrix(3, 1, [[-1], [0], [0]]))
+            if rt is True:
+                new_poly.add_to_center(Matrix(3, 1, [[1], [0], [0]]))
+
             canv.update()
         root.after(5, loop_rotation)
 
