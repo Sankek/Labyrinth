a8e4122bc057e1ee7e1a5ce6d5565f5baabb5b34
Sankek <iziurov.as@phystech.edu>
2019-11-24 05:56:56 +0300

    Changed matrices for left-right rotations
    Now it rotates properly around world vertical axis, not around camera's
    You can see the effect if you'll move right above the pyramid and will look down  and start rotating

--- main.py
+++ main.py
@@ -52,34 +52,94 @@ transform_d = Matrix(4, 4, [
     [-4, 0, 0, 1]
 ])
 
-a = 0.1
+transform_q = Matrix(4, 4, [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 4, 1]
+])
+
+transform_e = Matrix(4, 4, [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, -4, 1]
+])
+
+ang = 0.1
+
 transform_rot_up = Matrix(4, 4, [
     [1, 0,       0,      0],
-    [0, cos(a),  sin(a), 0],
-    [0, -sin(a), cos(a), 0],
+    [0, cos(ang),  sin(ang), 0],
+    [0, -sin(ang), cos(ang), 0],
     [0, 0,       0,      1]
 ])
 
 transform_rot_down = Matrix(4, 4, [
     [1, 0,        0,       0],
-    [0, cos(-a),  sin(-a), 0],
-    [0, -sin(-a), cos(-a), 0],
+    [0, cos(-ang),  sin(-ang), 0],
+    [0, -sin(-ang), cos(-ang), 0],
     [0, 0,        0,       1]
 ])
 
+
+# this two matrices rotate camera relative to the world vertical axis
+def tr_rot_right(phi):
+    global ang
+    c = cos(ang)
+    s = sin(ang)
+    anc = cos(phi)
+    ans = sin(phi)
+    return Matrix(4, 4, [
+        [c,      s*ans,          -s*anc,         0],
+        [-s*ans, anc**2*(1-c)+c, anc*ans*(1-c),  0],
+        [s*anc,  anc*ans*(1-c),  ans**2*(1-c)+c, 0],
+        [0,      0,              0,              1]
+    ])
+
+
+def tr_rot_left(phi):
+    global ang
+    c = cos(-ang)
+    s = sin(-ang)
+    anc = cos(phi)
+    ans = sin(phi)
+    return Matrix(4, 4, [
+        [c,      s*ans,          -s*anc,         0],
+        [-s*ans, anc**2*(1-c)+c, anc*ans*(1-c),  0],
+        [s*anc,  anc*ans*(1-c),  ans**2*(1-c)+c, 0],
+        [0,      0,              0,              1]
+    ])
+
+
 transform_rot_left = Matrix(4, 4, [
-    [cos(-a), 0, -sin(-a), 0],
+    [cos(-ang), 0, -sin(-ang), 0],
     [0,       1, 0,        0],
-    [sin(-a), 0, cos(-a),  0],
+    [sin(-ang), 0, cos(-ang),  0],
     [0,       0, 0,        1]
 ])
 
 transform_rot_right = Matrix(4, 4, [
-    [cos(a), 0, -sin(a), 0],
+    [cos(ang), 0, -sin(ang), 0],
     [0,      1, 0,       0],
-    [sin(a), 0, cos(a),  0],
+    [sin(ang), 0, cos(ang),  0],
     [0,      0, 0,       1]
 ])
+
+transform_rot_z1 = Matrix(4, 4, [
+    [cos(ang),  sin(ang), 0, 0],
+    [-sin(ang), cos(ang), 0, 0],
+    [0,       0,      1, 0],
+    [0,       0,      0, 1]
+])
+
+transform_rot_z2 = Matrix(4, 4, [
+    [cos(ang),  sin(ang), 0, 0],
+    [-sin(ang), cos(ang), 0, 0],
+    [0,       0,      1, 0],
+    [0,       0,      0, 1]
+])
+
 
 # coordinates of the view pyramid's vertices
 l, t, r, b = -40, 30, 40, -30
@@ -101,6 +161,7 @@ pyramid.toScreen()
 
 a, w, s, d, q, e = False, False, False, False, False, False
 rot_up, rot_down, rot_left, rot_right = False, False, False, False
+angle = 0.0
 
 
 def bindings():
@@ -125,6 +186,16 @@ def bindings():
         global d
         if not d:
             d = True
+
+    def move_q(event):
+        global q
+        if not q:
+            q = True
+
+    def move_e(event):
+        global e
+        if not e:
+            e = True
             
     def move_rot_up(event):
         global rot_up
@@ -162,6 +233,14 @@ def bindings():
         global d
         d = False
 
+    def stop_q(event):
+        global q
+        q = False
+
+    def stop_e(event):
+        global e
+        e = False
+
     def stop_rot_up(event):
         global rot_up
         rot_up = False
@@ -182,6 +261,8 @@ def bindings():
     root.bind('s', move_s)
     root.bind('a', move_a)
     root.bind('d', move_d)
+    root.bind('q', move_q)
+    root.bind('e', move_e)
     root.bind('<Up>', move_rot_up)
     root.bind('<Down>', move_rot_down)
     root.bind('<Left>', move_rot_left)
@@ -190,6 +271,8 @@ def bindings():
     root.bind('<KeyRelease-s>', stop_s)
     root.bind('<KeyRelease-a>', stop_a)
     root.bind('<KeyRelease-d>', stop_d)
+    root.bind('<KeyRelease-q>', stop_q)
+    root.bind('<KeyRelease-e>', stop_e)
     root.bind('<KeyRelease-Up>', stop_rot_up)
     root.bind('<KeyRelease-Down>', stop_rot_down)
     root.bind('<KeyRelease-Left>', stop_rot_left)
@@ -218,7 +301,7 @@ prims_list = [p1, p2, p3, p4, p5]
 
 
 def loop():
-    global a, w, s, d, rot_up, rot_down, rot_left, rot_right, prims_list
+    global a, w, s, d, q, e, rot_up, rot_down, rot_left, rot_right, prims_list, angle
     # This will only work properly if the initial position of the camera was left the same!!!
     if w:
         pyramid.toWorld(transform_w)
@@ -232,17 +315,25 @@ def loop():
     if d:
         pyramid.toWorld(transform_d)
 
-    if rot_up:
+    if q:
+        pyramid.toWorld(transform_q)
+
+    if e:
+        pyramid.toWorld(transform_e)
+
+    if rot_up and angle <= 1.5:
+        angle += 0.1
         pyramid.toWorld(transform_rot_up)
 
     if rot_left:
-        pyramid.toWorld(transform_rot_left)
+        pyramid.toWorld(tr_rot_left(angle))
 
-    if rot_down:
+    if rot_down and angle >= -1.5:
+        angle -= 0.1
         pyramid.toWorld(transform_rot_down)
 
     if rot_right:
-        pyramid.toWorld(transform_rot_right)
+        pyramid.toWorld(tr_rot_right(angle))
 
     pyramid.toCamera(matr_E(4))
 
